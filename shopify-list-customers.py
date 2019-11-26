2b1929ec30049defbcfa7f11b4b985b3d08226f3
David Z. Williams <dave@flex.io>
2019-11-26 16:29:57 -0500

    Updated property descriptions.

--- shopify-list-customers.py
+++ shopify-list-customers.py
@@ -12,40 +12,37 @@
 # examples:
 # notes: |
 #   The following properties are available:
-#     * `id`: TODO
-#     * `email`: TODO
-#     * `accepts_marketing`: TODO
-#     * `created_at`: TODO
-#     * `updated_at`: TODO
-#     * `first_name`: TODO
-#     * `last_name`: TODO
-#     * `orders_count`: TODO
-#     * `state`: TODO
-#     * `total_spent`: TODO
-#     * `last_order_id`: TODO
-#     * `note`: TODO
-#     * `verified_email`: TODO
-#     * `multipass_identifier`: TODO
-#     * `tax_exempt`: TODO
-#     * `phone`: TODO
-#     * `tags`: TODO
-#     * `last_order_name`: TODO
-#     * `currency`: TODO
-#     * `accepts_marketing_updated_at`: TODO
-#     * `marketing_opt_in_level`: TODO
-#     * `tax_exemptions`: TODO
-#     * `admin_graphql_api_id`: TODO
-#     * `addresses`: TODO
-#     * `default_address_address1`: TODO
-#     * `default_address_address2`: TODO
-#     * `default_address_city`: TODO
-#     * `default_address_company`: TODO
-#     * `default_address_country`: TODO
-#     * `default_address_province`: TODO
-#     * `default_address_province_code`: TODO
-#     * `default_address_zip`: TODO
-#     * `default_address_country_code`: TODO
-#     * `default_address_country_name`: TODO
+#     * `id`: A unique identifier for the customer.
+#     * `email`: The unique email address of the customer.
+#     * `accepts_marketing`: Whether the customer has consented to receive marketing material via email.
+#     * `created_at`: The date and time (ISO 8601 format) when the customer was created.
+#     * `updated_at`: The date and time (ISO 8601 format) when the customer information was last updated.
+#     * `first_name`: The customer's first name.
+#     * `last_name`: The customer's last name.
+#     * `orders_count`: The number of orders associated with this customer.
+#     * `state`: The state of the customer's account with a shop: **disabled**, **invited**, **enabled** or **declined**
+#     * `total_spent`: The total amount of money that the customer has spent across their order history.
+#     * `note`: A note about the customer.
+#     * `verified_email`: Whether the customer has verified their email address.
+#     * `tax_exempt`: Whether the customer is exempt from paying taxes on their order. If **true**, then taxes won't be applied to an order at checkout. If **false**, then taxes will be applied at checkout.
+#     * `phone`: The unique phone number (E.164 format) for this customer.
+#     * `tags`: Tags that the shop owner has attached to the customer, formatted as a string of comma-separated values.
+#     * `last_order_id`: The ID of the customer's last order.
+#     * `last_order_name`: The name of the customer's last order. This is directly related to the name field on the Order resource.
+#     * `currency`: The three-letter code (ISO 4217 format) for the currency that the customer used when they paid for their last order.
+#     * `accepts_marketing_updated_at`: The date and time (ISO 8601 format) when the customer consented or objected to receiving marketing material by email.
+#     * `marketing_opt_in_level`: The marketing subscription opt-in level (as described by the M3AAWG best practices guideline) that the customer gave when they consented to receive marketing material by email: **single_opt_in**, **confirmed_opt_in** or **unknown**
+#     * `tax_exemptions`: Whether the customer is exempt from paying specific taxes on their order. Canadian taxes only. See [Shopify customer properties](https://help.shopify.com/en/api/reference/customers/customer#index-2019-10) for a list of available values.
+#     * `address1`: The first line of the customer's mailing address.
+#     * `address2`: An additional field for the customer's mailing address.
+#     * `city`: The customer's city, town, or village.
+#     * `company`: The customer's company.
+#     * `country`: The customer's country.
+#     * `country_code`: The two-letter country code corresponding to the customer's country.
+#     * `country_name`: The customer's normalized country name.
+#     * `province`: The customer's region name. Typically a province, a state, or a prefecture.
+#     * `province_code`: The two-letter code for the customer's region.
+#     * `zip`: The customer's postal code, also known as zip, postcode, Eircode, etc.
 # ---
 
 import json
@@ -106,28 +103,30 @@ def flexio_handler(flex):
     property_map['orders_count'] = 'orders_count'
     property_map['state'] = 'state'
     property_map['total_spent'] = 'total_spent'
-    property_map['last_order_id'] = 'last_order_id'
     property_map['note'] = 'note'
     property_map['verified_email'] = 'verified_email'
-    property_map['multipass_identifier'] = 'multipass_identifier'
+    #property_map['multipass_identifier'] = 'multipass_identifier'
     property_map['tax_exempt'] = 'tax_exempt'
     property_map['phone'] = 'phone'
     property_map['tags'] = 'tags'
+    property_map['last_order_id'] = 'last_order_id'
     property_map['last_order_name'] = 'last_order_name'
     property_map['currency'] = 'currency'
     property_map['accepts_marketing_updated_at'] = 'accepts_marketing_updated_at'
     property_map['marketing_opt_in_level'] = 'marketing_opt_in_level'
     property_map['tax_exemptions'] = 'tax_exemptions'
-    property_map['default_address_address1'] = 'default_address.address1'
-    property_map['default_address_address2'] = 'default_address.address2'
-    property_map['default_address_city'] = 'default_address.city'
-    property_map['default_address_company'] = 'default_address.company'
-    property_map['default_address_country'] = 'default_address.country'
-    property_map['default_address_province'] = 'default_address.province'
-    property_map['default_address_province_code'] = 'default_address.province_code'
-    property_map['default_address_zip'] = 'default_address.zip'
-    property_map['default_address_country_code'] = 'default_address.country_code'
-    property_map['default_address_country_name'] = 'default_address.country_name'
+
+    # these all come from nested values in the 'default_address' object
+    property_map['address1'] = 'default_address.address1'
+    property_map['address2'] = 'default_address.address2'
+    property_map['city'] = 'default_address.city'
+    property_map['company'] = 'default_address.company'
+    property_map['country'] = 'default_address.country'
+    property_map['country_code'] = 'default_address.country_code'
+    property_map['country_name'] = 'default_address.country_name'
+    property_map['province'] = 'default_address.province'
+    property_map['province_code'] = 'default_address.province_code'
+    property_map['zip'] = 'default_address.zip'
 
     try:
 
