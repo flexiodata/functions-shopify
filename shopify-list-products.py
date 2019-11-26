62ac8807e0ca5e605c4b22791db5a8d7b8c2a45d
David Z. Williams <dave@flex.io>
2019-11-26 17:42:31 -0500

    Working on adding 'deep get' for arrays too. Added a bunch of properties from the first item of the 'variants' array.

--- shopify-list-products.py
+++ shopify-list-products.py
@@ -24,6 +24,23 @@
 #     * `template_suffix`: TODO
 #     * `tags`: TODO
 #     * `published_scope`: TODO
+#     * `product_id`: TODO
+#     * `price`: TODO
+#     * `sku`: TODO
+#     * `position`: TODO
+#     * `inventory_policy`: TODO
+#     * `compare_at_price`: TODO
+#     * `fulfillment_service`: TODO
+#     * `inventory_management`: TODO
+#     * `taxable`: TODO
+#     * `barcode`: TODO
+#     * `grams`: TODO
+#     * `weight`: TODO
+#     * `weight_unit`: TODO
+#     * `inventory_item_id`: TODO
+#     * `inventory_quantity`: TODO
+#     * `old_inventory_quantity`: TODO
+#     * `requires_shipping`: TODO
 # ---
 
 import json
@@ -74,18 +91,39 @@ def flexio_handler(flex):
 
     # map this function's property names to the API's property names
     property_map = OrderedDict()
-    property_map['id']                   = 'id'
-    property_map['title']                = 'title'
-    property_map['body_html']            = 'body_html'
-    property_map['vendor']               = 'vendor'
-    property_map['product_type']         = 'product_type'
-    property_map['created_at']           = 'created_at'
-    property_map['handle']               = 'handle'
-    property_map['updated_at']           = 'updated_at'
-    property_map['published_at']         = 'published_at'
-    property_map['template_suffix']      = 'template_suffix'
-    property_map['tags']                 = 'tags'
-    property_map['published_scope']      = 'published_scope'
+    property_map['id'] = 'id'
+    property_map['title'] = 'title'
+    property_map['body_html'] = 'body_html'
+    property_map['vendor'] = 'vendor'
+    property_map['product_type'] = 'product_type'
+    property_map['created_at'] = 'created_at'
+    property_map['handle'] = 'handle'
+    property_map['updated_at'] = 'updated_at'
+    property_map['published_at'] = 'published_at'
+    property_map['template_suffix'] = 'template_suffix'
+    property_map['tags'] = 'tags'
+    property_map['published_scope'] = 'published_scope'
+
+    # these all come from nested first element values in the 'variants' array
+    property_map['product_id'] = 'variants[0].product_id'
+    property_map['price'] = 'variants[0].price'
+    property_map['sku'] = 'variants[0].sku'
+    property_map['position'] = 'variants[0].position'
+    property_map['inventory_policy'] = 'variants[0].inventory_policy'
+    property_map['compare_at_price'] = 'variants[0].compare_at_price'
+    property_map['fulfillment_service'] = 'variants[0].fulfillment_service'
+    property_map['inventory_management'] = 'variants[0].inventory_management'
+    property_map['taxable'] = 'variants[0].taxable'
+    property_map['barcode'] = 'variants[0].barcode'
+    property_map['grams'] = 'variants[0].grams'
+    property_map['weight'] = 'variants[0].weight'
+    property_map['weight_unit'] = 'variants[0].weight_unit'
+    property_map['inventory_item_id'] = 'variants[0].inventory_item_id'
+    property_map['inventory_quantity'] = 'variants[0].inventory_quantity'
+    property_map['old_inventory_quantity'] = 'variants[0].old_inventory_quantity'
+    property_map['requires_shipping'] = 'variants[0].requires_shipping'
+
+
 
     try:
 
@@ -129,6 +167,34 @@ def flexio_handler(flex):
 
     except:
         raise RuntimeError
+
+def safe_list_get(_list, idx, default):
+    try:
+        return _list[idx]
+    except IndexError:
+        return default
+
+def deep_get(obj, path, default=None):
+    try:
+        keys = path.split('.')
+        for key in keys:
+            if key.find('[') >= 0:
+                parts = key.split('[')
+                key = parts[0]
+                obj = obj.get(key, default)
+                if isinstance(obj, list):
+                    idx = int(parts[1].strip(']'))
+                    obj = safe_list_get(obj, idx, default)
+                else:
+                    return default
+            if isinstance(obj, dict):
+                obj = obj.get(key, default)
+            else:
+                return default
+        return obj
+
+    except:
+        return default
 
 def validator_list(field, value, error):
     if isinstance(value, str):
